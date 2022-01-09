f066d7aba8c3d720cb1fea9b68493e8e41b5cffc
tinusboss <157947+tinusboss@users.noreply.github.com>
2022-01-09 20:18:43 +0100

    vastgelopen in laravel paginate

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+
+
+class PostController extends Controller
+{
+    public function index(){
+
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(
+                request(['search','category','author'])
+            )->paginator(6),
+        ]);
+    }
+    public function show(Post $post){
+        return view('posts.show',[
+            'post'=> $post
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,15 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-    Route::get('/', function () {
-        return view('posts',[
-            'posts' => Post::all()
-        ]);
-    });
+Route::get ('/', [\App\Http\Controllers\PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 
-//Find a post its slug and pass it to a view called "post"
-Route::get('posts/{post}',function ($slug){
-    return view('post',[
-        'post'=> Post::findOrFail($slug)
-    ]);
-});
 
