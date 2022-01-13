829a1d4809ee41dc5dae74f45231bb2e6d7faedc
tinusboss <157947+tinusboss@users.noreply.github.com>
2022-01-13 21:47:52 +0100

    voorbij 60

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+use http\Exception;
+use App\Services\Newsletter;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class NewsletterController extends Controller
+{
+
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Newsletter $newsletter)
+    {
+        request()->validate(['email' => 'required|email']);
+
+        try {
+            $newsletter->subscribe(request('email'));
+        } catch (Exception $e){
+            throw ValidationException::withMessages([
+                'email' => 'This email could not be added to our newsletter'
+            ]);
+
+        }
+
+        return redirect('/')
+            ->with('success', 'You are now signed up for our newsletter!');
+    }
+}
